cf430817888eaf3ebdb7a120d99c2fc1b377d37a
Pacequity <leonardovicelli@gmail.com>
2025-06-08 16:59:53 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './PAC1Dapp'
+import App from './PAC1Dapp.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
